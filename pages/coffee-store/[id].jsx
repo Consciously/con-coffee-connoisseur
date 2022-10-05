2f6cd745eb25a37276fac7caf6b8ed7430e0f855
Stefan Ihle <stefanihle@gmx.net>
2022-10-05 15:02:59 +0200

    add coffee-store page with dynamic route

---
+++ pages/coffee-store/[id].jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { useRouter } from 'next/router';
+
+const CoffeeStore = () => {
+	const router = useRouter();
+	const { id } = router.query;
+	return <div>CoffeeStore {id}</div>;
+};
+
+export default CoffeeStore;
