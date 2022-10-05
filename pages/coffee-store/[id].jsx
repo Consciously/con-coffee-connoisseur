6fe9633d834742f21b9e27a97b88dbd7972d906f
Stefan Ihle <stefanihle@gmx.net>
2022-10-05 20:47:08 +0200

    add hero image and global fonts and implement _document.js file

--- pages/coffee-store/[id].jsx
+++ pages/coffee-store/[id].jsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import { useRouter } from 'next/router';
+import Link from 'next/link';
 
 const CoffeeStore = () => {
 	const router = useRouter();
 	const { id } = router.query;
-	return <div>CoffeeStore {id}</div>;
+	return (
+		<div>
+			CoffeeStore {id}
+			<Link href='/'>
+				<a>Back to home</a>
+			</Link>
+			<Link href='/coffee-store/:id'>
+				<a>Go to page dynamic</a>
+			</Link>
+		</div>
+	);
 };
 
 export default CoffeeStore;
